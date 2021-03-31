21c465607d230123dbe31d372a29f5893c8617e0
SttDmitry <sheicko.dmitry@yandex.ru>
2021-03-31 23:02:11 +0300

    hw7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -3,7 +3,17 @@ package lesson7;
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(10);
-
+        graph.addEdge(0,2);
+        graph.addEdge(1,2);
+        graph.addEdge(3,1);
+        graph.addEdge(3,4);
+        graph.addEdge(3,5);
+        graph.addEdge(5,7);
+        graph.addEdge(4,6);
+        graph.addEdge(6, 8);
+        graph.addEdge(8,5);
+        graph.addEdge(8,3);
+        graph.addEdge(7,9);
 
 
 //        System.out.println(graph.getAdjList(4));
@@ -13,6 +23,8 @@ public class Main {
 //        System.out.println(dfp.pathTo(2));
 
         BreadthFirstPath bfp  = new BreadthFirstPath(graph, 0);
+        System.out.println(bfp.hasPathTo(9));
+        System.out.println(bfp.pathTo(9));
 
 
 
